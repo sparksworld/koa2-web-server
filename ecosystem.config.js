c0392e88695df1bcc506531eeebb86e75efd0db2
spark <228436652@qq.com>
2019-07-03 17:32:30 +0800

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,7 @@ module.exports = {
         error_file: './logs/error.log', // 错误日志
         max_memory_restart: '2G', // 超过多大内存自动重启，仅防止内存泄露有意义，需要根据自己的业务设置
         env: {
-            NODE_ENV: 'production',
-            "pm2_EXTRA_DISPLAY": "true"
+            NODE_ENV: 'production'
         },
         exec_mode: 'cluster', // 开启多线程模式，用于负载均衡
         instances: 'max', // 启用多少个实例，可用于负载均衡
